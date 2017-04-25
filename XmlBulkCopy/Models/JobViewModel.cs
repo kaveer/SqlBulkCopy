fac5115d8eb0206058e8fadc949e2f273387a013
kaveer <kaveer.rajcoomar@gmail.com>
2017-04-25 21:07:38 +0400

    Add project files.

---
+++ XmlBulkCopy/Models/JobViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace XmlBulkCopy.Models
+{
+    [Serializable, XmlRoot("jobs")]
+    public class JobViewModel
+    {
+        [XmlElement("job")]
+        public List<Job> Job { get; set; }
+    }
+
+    public class Job
+    {
+        [XmlElement("jobref")]
+        public int JobRef { get; set; }
+        [XmlElement("date")]
+        public string Date { get; set; }
+        [XmlElement("title")]
+        public string Title { get; set; }
+        [XmlElement("company")]
+        public string Company { get; set; }
+        [XmlElement("email")]
+        public string Email { get; set; }
+        [XmlElement("url")]
+        public string Url { get; set; }
+        [XmlElement("salarymin")]
+        public string SalaryMin { get; set; }
+        [XmlElement("salarymax")]
+        public string SalaryMax { get; set; }
+        [XmlElement("benefits")]
+        public string Benefits { get; set; }
+        [XmlElement("salary")]
+        public string Salary { get; set; }
+        [XmlElement("jobtype")]
+        public string JobType { get; set; }
+        [XmlElement("full_part")]
+        public string Full_Part { get; set; }
+        [XmlElement("salary_per")]
+        public string Salary_Per { get; set; }
+        [XmlElement("location")]
+        public string Location { get; set; }
+        [XmlElement("country")]
+        public string Country { get; set; }
+        [XmlElement("description")]
+        public string Description { get; set; }
+        [XmlElement("category")]
+        public string Category { get; set; }
+        [XmlElement("image")]
+        public string Image { get; set; }
+    }
+}
---
+++ XmlBulkCopy/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Serialization;
+using XmlBulkCopy.Models;
+
+namespace XmlBulkCopy
+{
+    class Program
+    {
+        static string connectionString = @"Data Source=DESKTOP-AODP2NL\SQL12FULL;Initial Catalog=Staging;Integrated Security=True";
+
+        static void Main(string[] args)
+        {
+            string[] jobSite = new string[] { "careersincatering.xml", "Chef-Jobs.xml", "Hotel-Jobs.xml", "Justjobs.xml", "Student-Jobs.xml" };
+
+            foreach (var item in jobSite)
+            {
+                if (item != "Justjobs.xml")
+                    continue;
+
+                string filePath = Path.Combine(@"C:\Users\kaveer\Desktop\XmlFeed\FeedLink", item);
+                JobViewModel record = new JobViewModel();
+
+                XmlSerializer serializer = new XmlSerializer(typeof(JobViewModel));
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    record = (JobViewModel)serializer.Deserialize(fileStream);
+                }
+
+                BulkCopyJobs(record.Job);
+            }
+
+
+        }
+
+        private static void BulkCopyJobs(List<Job> job)
+        {
+            DataTable tmpCvlibrary = ToDataTable<Job>(job);
+            using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connectionString))
+            {
+                bulkCopy.DestinationTableName = "dbo.mdCvLibrary";
+                try
+                {
+                    bulkCopy.WriteToServer(tmpCvlibrary);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Setting column names as Property names
+                dataTable.Columns.Add(prop.Name);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            return dataTable;
+        }
+    }
+}
